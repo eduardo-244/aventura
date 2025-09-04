f82fe902dcb14ddbc035062efc031556dd9218c4
eduardo-244 <eduardo.bunick.santos@escola.pr.gov.br>
2025-09-04 10:59:34 -0300

    ok

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const avanca = document. querySelectorAll('.btn-proximo');
- 
-avanca.for
+const avanca = document.querySelectorA11('btn.proximo');
+const reiniciarBtn = document.getElementById('btn-reiniciar');
+
+avanca.forEach(button =>
+button.addEventListener('click', function() {
+     const atual = document,querySelector('.ativo');
+     const proximoPasso = 'passo- '+this.getAttribute('data-proximo');
+
+atual.classList.remove('ativo');
+const proximoElemento= document.getElementById(proximoPasso);
+
+if(proximoElemento){
+    proximoElemento.classList.add('ativo');
+}else {
+    console.error(`Elemento com ID "${proximoPasso}" não encontrado.`)
+}
+
+});
+});
+
